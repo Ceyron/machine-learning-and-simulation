3b6959b9a72350e5fb228a990c629be94ea89807
GStechschulte <stechschulteg@gmail.com>
2022-06-30 18:43:09 +0200

    average log. probability of evidence and while loop for user input

--- english/probabilistic_machine_learning/contrib/vi_elbo_intuition_visual_torch.py
+++ english/probabilistic_machine_learning/contrib/vi_elbo_intuition_visual_torch.py
@@ -11,11 +11,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def plot_mixture(probs, x):
+def plot_mixture(probs, x, elbo, kl, evidence):
 
     sns.lineplot(x=x, y=probs[:, 0])
     sns.lineplot(x=x, y=probs[:, 1])
     plt.legend(['True Posterior', 'Surrogate Posterior'])
+    plt.title('${} = {} + {}$'.format(elbo, kl, evidence))
     plt.show()
 
 
@@ -36,7 +37,7 @@ def approximate_kl_divergence(p, q, size):
     sample_size = torch.Size([size])
     sample_set = p.sample(sample_size)
     
-    return torch.mean((p.log_prob(sample_set) - q.log_prob(sample_set)))
+    return torch.mean((p.log_prob(sample_set) - q.log_prob(sample_set))).numpy()
 
 
 def true_posterior(mixture_probs, mus, sigmas):
@@ -55,31 +56,34 @@ def surrogate_posterior(loc_approx, scale_approx):
 
 def main():
 
-    #mu_approx = torch.tensor(float(input('mu_approx = ')))
-    #sigma_approx = torch.tensor(float(input('sigma_approx = ')))
+    while True:
+        mu_approx = torch.tensor(float(input('mu_approx = ')))
+        sigma_approx = torch.tensor(float(input('sigma_approx = ')))
 
-    mu_approx = torch.tensor(0.)
-    sigma_approx = torch.tensor(1.)
+        mixture_probs = torch.tensor([0.3, 0.4, 0.3])
+        mus = torch.tensor([-1.3, 2.2, 4.0])
+        sigmas = torch.tensor([2.3, 1.5, 4.4])
+        x = torch.linspace(-5, 5, 100)
 
-    mixture_probs = torch.tensor([0.3, 0.4, 0.3])
-    mus = torch.tensor([-1.3, 2.2, 4.0])
-    sigmas = torch.tensor([2.3, 1.5, 4.4])
-    x = torch.linspace(-5, 5, 100)
+        p = true_posterior(mixture_probs, mus, sigmas)
+        prob_values_p = torch.exp2((p.log_prob(x))).reshape(-1, 1)
 
-    p = true_posterior(mixture_probs, mus, sigmas)
-    prob_values_p = torch.exp2((p.log_prob(x))).reshape(-1, 1)
+        q = surrogate_posterior(mu_approx, sigma_approx)
+        prob_values_q = torch.exp2((q.log_prob(x))).reshape(-1, 1)
 
-    q = surrogate_posterior(mu_approx, sigma_approx)
-    prob_values_q = torch.exp2((q.log_prob(x))).reshape(-1, 1)
+        all_prob_values = torch.hstack((prob_values_p, prob_values_q))
 
-    all_prob_values = torch.hstack((prob_values_p, prob_values_q))
-    plot_mixture(probs=all_prob_values, x=x)
+        mean_kl = approximate_kl_divergence(p, q, 100000)
+        evidence = torch.mean(torch.log2(prob_values_p))  
+        elbo = -mean_kl + evidence
+        plot_mixture(all_prob_values, x, elbo, -mean_kl, evidence)
 
-    mean_kl = approximate_kl_divergence(p, q, 100000)
-    print(mean_kl)
+        again = str(input('done: [y/n]: '))
 
-    #evidence = . . .  
-    #elbo = -mean_kl + evidence
+        if again == 'y':
+            break
+        else:
+            pass
 
 
 if __name__ == '__main__':
